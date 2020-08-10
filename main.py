285887bbbae99b91e0eae49e4b59fe1426546a31
Shane McDonough <shanemcdman@gmail.com>
2020-08-09 22:58:45 -0700

    Write documentation

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import string
 import keyboard
 
 def remap_on_str(s: str, n: int) -> None:
+    """
+    shift n places in s dataset
+    :s: string of characters to shift upon
+    :n: number of spaces to shift
+    """
     for i in range(len(s)):
         keyboard.remap_key(s[i], s[(i + n) % len(s)])
 
-def remap_to_ROTN(n):
+def remap_to_ROTN(n: int) -> None:
+    """
+    remap keyboard to work in ROTN for n
+    :n: number of spaces to shift
+    """
     exit_key = "shift+esc"
     remap_on_str(string.ascii_lowercase, n)
     remap_on_str(string.ascii_uppercase, n)
